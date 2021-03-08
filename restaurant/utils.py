8b71d22c9e07348bc4d2cc52e46da9aebe64b413
xiaodan-tang <shonna.x.tang@gmail.com>
2021-03-08 17:02:02 -0500

    fix get_filtered_restaurant test

--- restaurant/utils.py
+++ restaurant/utils.py
@@ -260,10 +260,11 @@ def get_filtered_restaurants(
     keyword_filter = {}
     if keyword:
         keyword_filter["restaurant_name__icontains"] = keyword
-    if "COVIDCompliant" in compliant:
-        keyword_filter["compliant_status__iexact"] = "Compliant"
-    if "MOPDCompliant" in compliant:
-        keyword_filter["mopd_compliance_status__iexact"] = "Compliant"
+    if compliant:
+        if "COVIDCompliant" in compliant:
+            keyword_filter["compliant_status__iexact"] = "Compliant"
+        if "MOPDCompliant" in compliant:
+            keyword_filter["mopd_compliance_status__iexact"] = "Compliant"
 
     value = None
     if sort_option:
