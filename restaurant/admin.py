f22b9dcb3e33aaa65fecdf9f06a6f3cc09752777
HahnNYU <hgh2023@nyu.edu>
2021-03-09 12:44:21 -0500

    temp MOPD data and model added

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import (
     InspectionRecords,
     Restaurant,
     UserQuestionnaire,
     YelpRestaurantDetails,
     Zipcodes,
+    AccessibilityRecord,
 )
 
 
@@ -13,3 +15,7 @@ admin.site.register(InspectionRecords)
 admin.site.register(UserQuestionnaire)
 admin.site.register(YelpRestaurantDetails)
 admin.site.register(Zipcodes)
+
+@admin.register(AccessibilityRecord)
+class ViewAdmin(ImportExportModelAdmin):
+    pass
