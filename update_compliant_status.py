9c10ef6e92355834001224aa6f69509290b6657a
xiaodan-tang <shonna.x.tang@gmail.com>
2021-04-09 11:57:57 -0400

    get latest covid compliant status

--- update_compliant_status.py
+++ update_compliant_status.py
@@ -1,22 +1,25 @@
 import os
 import django
-from restaurant.models import Restaurant
-from restaurant.utils import get_latest_inspection_record
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dinesafelysite.settings")
 django.setup()
+
+from restaurant.models import Restaurant, InspectionRecords  # noqa: E402
 
 
 def update_covid_compliant_status():
     restaurants = Restaurant.objects.all()
     for r in restaurants:
-        compliant_status = get_latest_inspection_record(
-            business_name=r.restaurant_name,
+        inspection_records = InspectionRecords.objects.filter(
+            restaurant_name=r.restaurant_name,
             business_address=r.business_address,
             postcode=r.postcode,
-        )["is_roadway_compliant"]
-        r.compliant_status = compliant_status
-        r.save()
+        ).order_by("-inspected_on")
+
+        if len(inspection_records) >= 1:
+            latest_compliant_status = inspection_records[0].is_roadway_compliant
+            r.compliant_status = latest_compliant_status
+            r.save()
 
 
 if __name__ == "__main__":
